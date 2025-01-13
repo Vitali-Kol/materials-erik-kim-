3cceed7c49acf35d7f5e4819641b90d05762a199
Vitali Kolesnikov <vitali.kolesnikov@ivkhk.ee>
2025-01-13 11:22:12 +0200

    re

--- src/main/java/ee/spring/materials/ProductListController.java
+++ src/main/java/ee/spring/materials/ProductListController.java
@@ -1,6 +1,7 @@
 package ee.spring.materials;
 
 import ee.spring.materials.models.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class ProductListController {
     List<Product> products = new ArrayList<>(Arrays.asList(
