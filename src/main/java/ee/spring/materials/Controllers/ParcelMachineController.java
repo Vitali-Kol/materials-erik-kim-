f0b303f60a4b7fd9cacf8b2ea7204499c459c888
Vitali Kolesnikov <vitali.kolesnikov@ivkhk.ee>
2025-01-24 10:24:59 +0200

    Add files via upload

---
+++ src/main/java/ee/spring/materials/Controllers/ParcelMachineController.java
@@ -0,0 +1,25 @@
+package ee.spring.materials.Controllers;
+
+import ee.spring.materials.models.ParcelMachine;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin("http://localhost:3001")
+@RestController
+public class ParcelMachineController {
+
+    @GetMapping("parcel-machines")
+    public ParcelMachine[] getParcelMachines() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ParcelMachine[]> response = restTemplate
+                .exchange("https://www.omniva.ee/locations.json",
+                        HttpMethod.GET, null, ParcelMachine[].class);
+
+        return response.getBody();
+    }
+}
---
+++ src/main/java/ee/spring/materials/models/ParcelMachine.java
@@ -0,0 +1,204 @@
+package ee.spring.materials.models;
+
+public class ParcelMachine {
+    public String ZIP;
+
+    public String NAME;
+
+    public String TYPE;
+
+    public String A0_NAME;
+
+    public String A1_NAME;
+
+    public String A2_NAME;
+
+    public String A3_NAME;
+
+    public String A4_NAME;
+
+    public String A5_NAME;
+
+    public String A6_NAME;
+
+    public String A7_NAME;
+
+    public String A8_NAME;
+
+    public String X_COORDINATE;
+
+    public String Y_COORDINATE;
+
+    public String SERVICE_HOURS;
+
+    public String TEMP_SERVICE_HOURS;
+
+    public String TEMP_SERVICE_HOURS_UNTIL;
+
+    public String TEMP_SERVICE_HOURS_2;
+
+    public String TEMP_SERVICE_HOURS_2_UNTIL;
+
+    public String comment_est;
+
+    public String comment_eng;
+
+    public String comment_rus;
+
+    public String comment_lav;
+
+    public String comment_lit;
+
+    public String MODIFIED;
+
+    public void setZIP(String ZIP){
+        this.ZIP = ZIP;
+    }
+    public String getZIP(){
+        return this.ZIP;
+    }
+    public void setNAME(String NAME){
+        this.NAME = NAME;
+    }
+    public String getNAME(){
+        return this.NAME;
+    }
+    public void setTYPE(String TYPE){
+        this.TYPE = TYPE;
+    }
+    public String getTYPE(){
+        return this.TYPE;
+    }
+    public void setA0_NAME(String A0_NAME){
+        this.A0_NAME = A0_NAME;
+    }
+    public String getA0_NAME(){
+        return this.A0_NAME;
+    }
+    public void setA1_NAME(String A1_NAME){
+        this.A1_NAME = A1_NAME;
+    }
+    public String getA1_NAME(){
+        return this.A1_NAME;
+    }
+    public void setA2_NAME(String A2_NAME){
+        this.A2_NAME = A2_NAME;
+    }
+    public String getA2_NAME(){
+        return this.A2_NAME;
+    }
+    public void setA3_NAME(String A3_NAME){
+        this.A3_NAME = A3_NAME;
+    }
+    public String getA3_NAME(){
+        return this.A3_NAME;
+    }
+    public void setA4_NAME(String A4_NAME){
+        this.A4_NAME = A4_NAME;
+    }
+    public String getA4_NAME(){
+        return this.A4_NAME;
+    }
+    public void setA5_NAME(String A5_NAME){
+        this.A5_NAME = A5_NAME;
+    }
+    public String getA5_NAME(){
+        return this.A5_NAME;
+    }
+    public void setA6_NAME(String A6_NAME){
+        this.A6_NAME = A6_NAME;
+    }
+    public String getA6_NAME(){
+        return this.A6_NAME;
+    }
+    public void setA7_NAME(String A7_NAME){
+        this.A7_NAME = A7_NAME;
+    }
+    public String getA7_NAME(){
+        return this.A7_NAME;
+    }
+    public void setA8_NAME(String A8_NAME){
+        this.A8_NAME = A8_NAME;
+    }
+    public String getA8_NAME(){
+        return this.A8_NAME;
+    }
+    public void setX_COORDINATE(String X_COORDINATE){
+        this.X_COORDINATE = X_COORDINATE;
+    }
+    public String getX_COORDINATE(){
+        return this.X_COORDINATE;
+    }
+    public void setY_COORDINATE(String Y_COORDINATE){
+        this.Y_COORDINATE = Y_COORDINATE;
+    }
+    public String getY_COORDINATE(){
+        return this.Y_COORDINATE;
+    }
+    public void setSERVICE_HOURS(String SERVICE_HOURS){
+        this.SERVICE_HOURS = SERVICE_HOURS;
+    }
+    public String getSERVICE_HOURS(){
+        return this.SERVICE_HOURS;
+    }
+    public void setTEMP_SERVICE_HOURS(String TEMP_SERVICE_HOURS){
+        this.TEMP_SERVICE_HOURS = TEMP_SERVICE_HOURS;
+    }
+    public String getTEMP_SERVICE_HOURS(){
+        return this.TEMP_SERVICE_HOURS;
+    }
+    public void setTEMP_SERVICE_HOURS_UNTIL(String TEMP_SERVICE_HOURS_UNTIL){
+        this.TEMP_SERVICE_HOURS_UNTIL = TEMP_SERVICE_HOURS_UNTIL;
+    }
+    public String getTEMP_SERVICE_HOURS_UNTIL(){
+        return this.TEMP_SERVICE_HOURS_UNTIL;
+    }
+    public void setTEMP_SERVICE_HOURS_2(String TEMP_SERVICE_HOURS_2){
+        this.TEMP_SERVICE_HOURS_2 = TEMP_SERVICE_HOURS_2;
+    }
+    public String getTEMP_SERVICE_HOURS_2(){
+        return this.TEMP_SERVICE_HOURS_2;
+    }
+    public void setTEMP_SERVICE_HOURS_2_UNTIL(String TEMP_SERVICE_HOURS_2_UNTIL){
+        this.TEMP_SERVICE_HOURS_2_UNTIL = TEMP_SERVICE_HOURS_2_UNTIL;
+    }
+    public String getTEMP_SERVICE_HOURS_2_UNTIL(){
+        return this.TEMP_SERVICE_HOURS_2_UNTIL;
+    }
+    public void setComment_est(String comment_est){
+        this.comment_est = comment_est;
+    }
+    public String getComment_est(){
+        return this.comment_est;
+    }
+    public void setComment_eng(String comment_eng){
+        this.comment_eng = comment_eng;
+    }
+    public String getComment_eng(){
+        return this.comment_eng;
+    }
+    public void setComment_rus(String comment_rus){
+        this.comment_rus = comment_rus;
+    }
+    public String getComment_rus(){
+        return this.comment_rus;
+    }
+    public void setComment_lav(String comment_lav){
+        this.comment_lav = comment_lav;
+    }
+    public String getComment_lav(){
+        return this.comment_lav;
+    }
+    public void setComment_lit(String comment_lit){
+        this.comment_lit = comment_lit;
+    }
+    public String getComment_lit(){
+        return this.comment_lit;
+    }
+    public void setMODIFIED(String MODIFIED){
+        this.MODIFIED = MODIFIED;
+    }
+    public String getMODIFIED(){
+        return this.MODIFIED;
+    }
+}
